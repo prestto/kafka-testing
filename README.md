031f745a61bb3721245bf80150e6809e09d8fead
Tom PRESTON <tom.preston@octoenergy.com>
2023-12-28 12:32:51 +0000

    Add install instructions

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Kafka testing
 
 This repo serves as a for kafka with different services.
+
+## Dev
+
+If you'd like to clone and run the project, this section explains how to do so (bear in mind that the project was developed on MacOS Sonoma 14.2.1).
+
+### Install
+
+The following dependencies must be installed to run in local:
+
+- [minikube](https://minikube.sigs.k8s.io/docs/start/)
+- [Kubectl](https://v1-18.docs.kubernetes.io/docs/tasks/tools/install-kubectl/)
+- [Tilt](https://tilt.dev/)
+- [Docker](https://www.docker.com/)
+
+Run `./run.sh install` to install these.
---
+++ run.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+cecho() {
+    LB='\033[1;36m' # Light Blue
+    LG='\033[1;32m' # Light Green
+    YE='\033[1;33m' # Yellow
+
+    # print normally if no second arg added
+    if [[ $2 == "" ]]; then
+        echo $1
+        return
+    fi
+
+    case $1 in
+    BL)
+        printf "$LB%s\033[0m\n" "$2" # Light Cyan
+        ;;
+    GR)
+        printf "$LG%s\033[0m\n" "$2" # Light green
+        ;;
+    YE)
+        printf "$YE%s\033[0m\n" "$2" # Light green
+        ;;
+    normal | *)
+        echo $2 # Light Purple
+        ;;
+    esac
+}
+
+function run_install {
+    cecho "BL" "Installing system dependancy: Colima..."
+    brew install colima
+    cecho "BL" "Colima installed."
+    
+    cecho "BL" "Installing system dependancy: Docker..."
+    brew install docker
+    cecho "BL" "Docker installed."
+
+    cecho "BL" "Installing system dependancy: Minikube..."
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+    sudo install minikube-darwin-amd64 /usr/local/bin/minikube
+    cecho "BL" "Minikube installed."
+
+    cecho "BL" "Installing system dependancy: Tilt..."
+    curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
+    cecho "BL" "Tilt installed."
+}
+
+function show_help {
+    cecho "BL" "Help: $0 <ACTION>"
+    cecho "BL" "Parameters :"
+    cecho "BL" " - ACTION values :"
+    cecho "BL" "   * install                            - Install system deps."
+}
+
+
+if [[ "$1" == "" ]]; then
+    cecho "YE" "No arguments provided."
+    show_help
+    exit 1
+fi
+
+case "$1" in
+install)
+    run_install
+    ;;
+*)
+    show_help
+    ;;
+esac
